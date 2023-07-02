518ce513839b869aa73af2f9434a922977a4fe7e
dneog <debashisneog23@gmail.com>
2023-07-02 20:59:03 +0530

    Dynamic routing

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+function MeetupDetails(){
+    return(
+        <>
+            <h1>A First Meetup</h1>
+            <address>City-abc</address>
+            <p>This is the first Meetup</p>
+        </>
+    )
+}
+export default MeetupDetails
